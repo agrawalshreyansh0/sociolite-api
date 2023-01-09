d69adacb44dbcee8f2d62ce82c8449a10ca0d7a5
Shreyansh Agrawal <ashreyansh47@gmail.com>
2023-01-09 22:32:50 +0530

    added the destroy post

--- src/controller/post_controller.js
+++ src/controller/post_controller.js
@@ -1,23 +1,37 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment'); 
 
 module.exports.createPost = async (req, res) => {
     try {
-        await Post.create(req.body); 
-        console.log(`new post created by :${req.body.email}`); 
-        return res.json({ success: true, message: "Post created" }); 
+        await Post.create(req.body);
+        console.log(`new post created by :${req.body.email}`);
+        return res.json({ success: true, message: "Post created" });
     } catch (err) {
-        console.log(`Error in creating the post`); 
-        return res.json({ success: false, message: "Error in creating the post" }); 
+        console.log(`Error in creating the post`);
+        return res.json({ success: false, message: "Error in creating the post" });
     }
 }
 
 module.exports.getAllPosts = async (req, res) => {
     try {
-        const posts = await Post.find({}).populate('user').exec(); 
-        console.log(`got all posts`); 
-        return res.json({ success: true, posts }); 
+        const posts = await Post.find({}).populate('user').exec();
+        console.log(`got all posts`);
+        return res.json({ success: true, posts });
     } catch (error) {
-        console.log(`Error in getting all posts`); 
-        return res.json({ success: false, message: "Error in getting all posts" }); 
+        console.log(`Error in getting all posts`);
+        return res.json({ success: false, message: "Error in getting all posts" });
+    }
+}
+
+module.exports.destroy = async (req, res) => {
+    try {
+        const reqpost = await Post.findById(req.params.id);
+        await Comment.deleteMany({ post: req.params.id }); 
+        reqpost.remove(); 
+        console.log(`Post Delelted with all the comments`); 
+        return res.json({ success: true, message: "Post Deleted Successfully" });
+    } catch (err) {
+        console.log(`Error Detected`); 
+        return res.json({ success: false, message: "Error Detected" });
     }
 }
--- src/routers/post_router.js
+++ src/routers/post_router.js
@@ -3,5 +3,6 @@ const postController = require('../controller/post_controller');
 
 router.post('/createPost', postController.createPost);
 router.get('/allPosts', postController.getAllPosts);
+router.delete('/destroy/:id', postController.destroy);
 
 module.exports = router; 
