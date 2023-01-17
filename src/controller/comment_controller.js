703462c815c67496a966281b758027e3a4a919d9
Shreyansh Agrawal <ashreyansh47@gmail.com>
2023-01-17 12:51:37 +0530

    added the get all comments by post id route

--- src/controller/comment_controller.js
+++ src/controller/comment_controller.js
@@ -1,6 +1,6 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
-const Like = require('../models/like'); 
+const Like = require('../models/like');
 
 module.exports.createComment = async (req, res) => {
     try {
@@ -24,12 +24,32 @@ module.exports.destroy = async (req, res) => {
     try {
         let currComment = await Comment.findById(req.params.id);
         await Post.findByIdAndUpdate(currComment.post, { $pull: { comments: req.params.id } });
-        await Like.deleteMany({ likable: currComment._id, onModel: 'comment' }); 
+        await Like.deleteMany({ likable: currComment._id, onModel: 'comment' });
         currComment.remove();
         console.log(`Deleted the post`);
         return res.json({ success: true, message: "Deleted the post successfully" });
     } catch (error) {
         console.log(`error : `, error);
         return res.json({ success: false, message: "Error Detected" });
+    }
+}
+
+module.exports.getCommentsByPostId = async (req, res) => {
+    try {
+        let post = await Post
+            .findById(req.params.id)
+            .populate({ path: 'comments', populate: { path: 'user' } })
+            .exec();
+        if (post) {
+            let comments = post.comments;
+            console.log(`got the comments`);
+            return res.json({ success: true, data: comments });
+        } else {
+            console.log(`post not found with id : ${req.params.id}`);
+            return res.json({ success: false, message: "no such post found" });
+        }
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: `Error Detected` });
     }
 }
--- src/routers/comment_router.js
+++ src/routers/comment_router.js
@@ -3,5 +3,6 @@ const commentComtroller = require('../controller/comment_controller')
 
 router.post('/createComment', commentComtroller.createComment);
 router.delete('/destroy/:id', commentComtroller.destroy); 
+router.get('/getComments/:id', commentComtroller.getCommentsByPostId); 
 
 module.exports = router; 
