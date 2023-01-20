1f50de62fa971ad5157bf990b8dd68467719b345
Shreyansh Agrawal <ashreyansh47@gmail.com>
2023-01-20 16:22:20 +0530

    added the auth require

--- src/routers/user_router.js
+++ src/routers/user_router.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const passport = require('passport')
 const userController = require('../controller/user_controller');
 const user = require('../models/user');
+const auth = require('../config/auth'); 
 
 router.post('/signIn', userController.signIn);
 router.post('/createUser', userController.createUser);
