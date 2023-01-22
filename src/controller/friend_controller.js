bfac457bc484f88f371806ba92bc53cf21356692
Shreyansh Agrawal <ashreyansh47@gmail.com>
2023-01-22 22:44:51 +0530

    another mistake

--- src/controller/friend_controller.js
+++ src/controller/friend_controller.js
@@ -5,7 +5,8 @@ module.exports.sendRequest = async (req, res) => {
     try {
         const user = await User.findById(req.body.recieverId);
         const sender = await User.findById(req.body.senderId);
-        user.requestRecieved.push(sender._id);
+        console.log("found users"); 
+        user.requestsRecieved.push(sender._id);
         sender.requestsSent.push(user._id);
         user.save();
         sender.save();
@@ -21,7 +22,7 @@ module.exports.acceptRequest = async (req, res) => {
     try {
         const reciever = await User.findById(req.body.recieverId);
         const sender = await User.findById(req.body.senderId); 
-        reciever.requestRecieved.pull(req.body.senderId);
+        reciever.requestsRecieved.pull(req.body.senderId);
         reciever.friends.add(req.body.senderId);
         reciever.save();
         sender.requestsSent.pull(req.body.recieverId);
