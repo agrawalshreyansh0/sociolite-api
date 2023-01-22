ce8bd4928ba82bd8d5039c4ff364ca194e783809
Shreyansh Agrawal <ashreyansh47@gmail.com>
2023-01-22 22:49:43 +0530

    corrected accept requrest

--- src/controller/friend_controller.js
+++ src/controller/friend_controller.js
@@ -5,12 +5,11 @@ module.exports.sendRequest = async (req, res) => {
     try {
         const user = await User.findById(req.body.recieverId);
         const sender = await User.findById(req.body.senderId);
-        console.log("found users"); 
         user.requestsRecieved.push(sender._id);
         sender.requestsSent.push(user._id);
         user.save();
         sender.save();
-        console.log(`request sent from :${sender.email} to ${user.email}`);
+        console.log(`request sent from :${sender.id} to ${user.id}`);
         return res.json({ success: true, message: "request sent" });
     } catch (error) {
         console.log(error);
@@ -21,13 +20,15 @@ module.exports.sendRequest = async (req, res) => {
 module.exports.acceptRequest = async (req, res) => {
     try {
         const reciever = await User.findById(req.body.recieverId);
-        const sender = await User.findById(req.body.senderId); 
+        const sender = await User.findById(req.body.senderId);
         reciever.requestsRecieved.pull(req.body.senderId);
         reciever.friends.add(req.body.senderId);
         reciever.save();
         sender.requestsSent.pull(req.body.recieverId);
         sender.friends.add(req.body.recieverId);
         sender.save();
+        console.log(`friend request accepted by :${reciever.id} from ${sender.id}`);
+        return res.json({ success: true, message: "request accepted" });
     } catch (error) {
         console.log(error);
         return res.json({ success: false, message: "Error Detected" });
