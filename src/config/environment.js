4b6542c9874127a5c09ea7f9e79aae1c02628800
Shreyansh Agrawal <ashreyansh47@gmail.com>
2023-03-27 19:09:27 +0530

    added chat server at PORT

--- src/config/environment.js
+++ src/config/environment.js
@@ -5,8 +5,8 @@ const development = {
   jwt_key: "sociolite",
   bcrypt_salt: 7,
   jwt_expiry: "10000000",
-  PORT: process.env.PORT || 5000,
-  chatPort: process.env.ChatPORT || 5001,
+  PORT: process.env.extraPort || 5000,
+  chatPort: process.env.PORT || 5001,
 };
 
 //Remember: Put all the constants in the environment variables of the machine while deployment
